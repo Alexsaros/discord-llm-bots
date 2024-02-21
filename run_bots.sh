ac39aefe391b4ca7a1661af925533d28d52626dc
Alexsaros <153434642+Alexsaros@users.noreply.github.com>
2024-02-21 21:00:36 +0100

    Actually fixed the "models;C" folder being created on Windows this time.

--- run_bots.sh
+++ run_bots.sh
@@ -4,10 +4,10 @@ git pull
 git submodule update --init --recursive
 
 docker build -t discord-bots .
-if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+if [ "$(uname -s)" == "Linux" ]; then
     # For Linux
     docker run -v "$(pwd)"/models:/app/text-generation-webui/models discord-bots
 else
     # For Windows
-    docker run -v "%cd%/models:/app/text-generation-webui/models" discord-bots
+    docker run -v "/${PWD}/models:/app/text-generation-webui/models" discord-bots
 fi
