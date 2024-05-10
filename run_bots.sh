6f8a05f3e431df4d5e39f647f5395757f6db7106
Alexsaros <153434642+Alexsaros@users.noreply.github.com>
2024-05-10 19:26:43 +0200

    Automatically comment out a line containing "git pull" in a text-generation-webui file to prevent errors.

--- run_bots.sh
+++ run_bots.sh
@@ -3,6 +3,20 @@
 git pull
 git submodule update --init --recursive
 
+# Path to the one_click Python file
+python_file="text-generation-webui/one_click.py"
+# Check if the file exists
+if [ -f "$python_file" ]; then
+    # Use sed to comment out the line containing "git pull --autostash" if it hasn't been commented out yet
+    # This prevent the text-generation-webui from trying to pull from git
+    if ! grep -q "#.*git pull --autostash" "$python_file"; then
+        sed -i '/git pull --autostash/s/^/# /' "$python_file"
+        echo "Line 271 in $python_file has been commented out."
+    fi
+else
+    echo "Error: $python_file not found."
+fi
+
 docker build -t discord-bots .
 if [ "$(uname -s)" == "Linux" ]; then
     # For Linux
